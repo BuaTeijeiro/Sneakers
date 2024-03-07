d5ac93a4c3c00113d113c25cfdc2ed6df3dd680b
davidbua <bua.teijeiro.david@gmail.com>
2024-03-07 12:36:23 +0100

    refactor(Bids.java): Cambiar bucles por streams

--- app/src/main/java/edu/sneakers/criteria/Bids.java
+++ app/src/main/java/edu/sneakers/criteria/Bids.java
@@ -1,18 +1,19 @@
 package edu.sneakers.criteria;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import edu.sneakers.items.Bid;
+import java.util.List;
+import java.util.Comparator;
+
 import edu.sneakers.items.Item;
 import edu.sneakers.items.Offer;
+import edu.sneakers.items.Bid;
+
 
 public class Bids implements Criteria{
     public Bids(){
         //Je to jen prázdný, zádný problém
     }
-
-    @Override
+    /*     @Override
     public List<Offer> checkCriteria(Item item) {
         ArrayList<Offer> offers = item.offers();
         List<Offer> filteredOffers = new ArrayList<>();
@@ -22,6 +23,10 @@ public class Bids implements Criteria{
             }
         }
         return filteredOffers;
+    }*/
+    @Override 
+    public List<Offer> checkCriteria(Item item) {
+        return item.offers().stream().filter(a -> a instanceof Bid).sorted(Comparator.reverseOrder()).toList();
     }
 
 }
